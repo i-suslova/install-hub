7b14b9e389dfbfe13214eb4284e9b463f780751e
Irina <i.sus2011@yandex.ru>
2026-03-17 21:02:53 +0300

    add sitemap.xml robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/install-hub/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/install-hub/",
+  base: "/",
 });
